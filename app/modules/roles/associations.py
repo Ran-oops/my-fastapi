bb70c29d269270011fc13289e1f056a551dc4f46
Ran <3460466167@qq.com>
2026-03-25 11:11:16 +0800

    feat(roles): add associations.py with role_permissions table

---
+++ app/modules/roles/associations.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, ForeignKey, Integer, Table
+
+from app.db.base import UserBase
+
+role_permissions = Table(
+    "role_permissions",
+    UserBase.metadata,
+    Column("role_id", Integer, ForeignKey("roles.id"), primary_key=True),
+    Column("permission_id", Integer, ForeignKey("permissions.id"), primary_key=True),
+)
